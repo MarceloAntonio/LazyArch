73c223f59e8929f82397027e5a0b9ac438af29bd
Marcelo Antonio <marcelomartins.escorpiao2005@gmail.com>
2026-03-13 16:29:11 -0300

    UI hub improved and running as sudo bug fixed

--- Hub_Installer_Arch/Hub.py
+++ Hub_Installer_Arch/Hub.py
@@ -3,7 +3,7 @@ from . import EncerrarPrograma, InstalarAUR, InstalarNeoVimPlus, TrocarShell
 #Função Hub, função que pede a opção e retorna a função pedida
 def Hub():
     while(True):
-        hubChoice = int(input("HUB\nEscolha uma das opções a baixo\n[1] - Instalar AUR\n[2] - Instalar & trocar shell\nInstalar nvim personalizado\n\n: "))
+        hubChoice = int(input("HUB\nEscolha uma das opções a baixo\n[1] - Instalar AUR\n[2] - Instalar & trocar shell\n[3] - Instalar nvim personalizado\n[4] - Sair\n: "))
         match hubChoice:
              case 1:
                    InstalarAUR.InstalarAUR()
--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from Hub_Installer_Arch import Hub, VerificarPrivilegio, EncerrarPrograma
 
 #Função central que inicia o sistema
 def Main():
-     if VerificarPrivilegio.VerificarPrivilegio == True:
+     if VerificarPrivilegio.VerificarPrivilegio() == True:
           EncerrarPrograma.EncerrarPrograma("Rode o script sem sudo ou root")
      else:
           Hub.Hub()
