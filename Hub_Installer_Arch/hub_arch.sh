52a9c20e23de9b68622eb889c5c66b31173e65e4
Marcelo Antonio <marcelomartins.escorpiao2005@gmail.com>
2026-03-19 10:47:43 -0300

    fix some path bugs from scripts

--- Hub_Installer_Arch/hub_arch.sh
+++ Hub_Installer_Arch/hub_arch.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-checar_pip() {
-    # Verifica se o executável 'pip' existe no sistema
-    if ! command -v pip &> /dev/null; then
-        echo "Pip não encontrado. Instalando..."
-        sudo pacman -S python-pip --noconfirm --needed
-    else
-        echo ""
-    fi
-}
 
-
-checar_python() {
-    if pacman -Qi python &> /dev/null; then
-        echo ""
-    else
-        echo "Python não instalado"
-        echo "instalando Python"
-        sudo pacman -S python --noconfirm --needed
-    fi
-}
-
-checar_python
-checar_pip
 
 sudo pip install -r requirements.txt --break-system-packages
 python main.py
--- Install.sh
+++ Install.sh
@@ -2,6 +2,7 @@
 
 
 script="./Hub_Installer_Arch/hub_arch.sh"
+dependencies="./Hub_Installer_Arch/requirements.txt"
 
 
 if [ ! -f "$script" ]; then
@@ -11,6 +12,32 @@ fi
 
 
 sudo chmod +x "$script"
+
+checar_pip() {
+    # Verifica se o executável 'pip' existe no sistema
+    if ! command -v pip &> /dev/null; then
+        echo "Pip não encontrado. Instalando..."
+        sudo pacman -S python-pip --noconfirm --needed
+    else
+        echo ""
+    fi
+}
+
+
+checar_python() {
+    if pacman -Qi python &> /dev/null; then
+        echo ""
+    else
+        echo "Python não instalado"
+        echo "instalando Python"
+        sudo pacman -S python --noconfirm --needed
+    fi
+}
+
+checar_python
+checar_pip
+
+
 
 
 sudo mkdir -p /usr/local/bin/hub_arch_files
