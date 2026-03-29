ee82d525cc0d8ecdb63022ed69ddb4b872633da2
Marcelo Antonio <marcelomartins.escorpiao2005@gmail.com>
2026-03-29 19:48:25 -0300

    Testing

--- LazyArch/LazyArch/install_aur.py
+++ LazyArch/LazyArch/install_aur.py
@@ -18,10 +18,10 @@ def install_aur():
           subprocess.run(f"git clone https://aur.archlinux.org/yay.git {install_dir}", shell=True)
 
           print("\n\n# Entering the folder yay #\n\n")
-          subprocess.run(f"cd {install_dir}", shell=True)
+          subprocess.run(f"", shell=True)
 
           print("\n\n# Running the initial installation #\n\n")
-          subprocess.run("makepkg -si", shell=True)
+          subprocess.run(f"cd {install_dir} && makepkg -si", shell=True)
 
           print("\n\n# Cleaning cache #\n\n")
           subprocess.run(f"sudo rm -R {install_dir}", shell=True)
